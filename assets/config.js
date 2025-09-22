2b20cc4080392f26f7ef503fc8045cc8b04f4926
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2025-09-23 00:25:36 +0200

    Create config.js

---
+++ assets/config.js
@@ -0,0 +1,25 @@
+// assets/config.js
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD4gJfp1166c_y3Q1--NQLJ_pLhhh_k-8",
+  authDomain: "ferienhaus-belegungsplan.firebaseapp.com",
+  projectId: "ferienhaus-belegungsplan",
+  storageBucket: "ferienhaus-belegungsplan.appspot.com",
+  messagingSenderId: "268588720447",
+  appId: "1:268588720447:web:c71b1b7526c722b71e738a"
+};
+
+// App Check Site Key (den bekommst du gleich, wenn du App Check aktivierst)
+export const appCheckSiteKey = "YOUR_APPCHECK_SITE_KEY";
+
+// Whitelist der Besitzer-E-Mails
+export const ownerWhitelist = [
+  "p.schmidt81@gmail.com",
+  "markus.rister@t-online.de"
+];
+
+// Action URL für den Magic-Link-Login
+export const actionCodeSettings = {
+  url: "https://phishman81.github.io/ferienhaus-belegungsplan/",
+  handleCodeInApp: true
+};
